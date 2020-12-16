4e8d771894063a80102ab8070b28af45a4aa7689
Nedelcho Nedelchev <snaiperistz@hotmail.com>
2020-12-16 05:19:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace EssentiBot
+{
+    class Program
+    {
+        public static async Task Main(string[] args)       
+            => await Startup.RunAsync(args);        
+    }
+}
