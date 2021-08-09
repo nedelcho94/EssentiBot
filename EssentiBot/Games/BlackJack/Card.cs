14b803a05c988d153188f081b67e7acc1eb1ec69
Nedelcho Nedelchev <snaiperistz@hotmail.com>
2021-08-09 21:17:51 +0300

    added blackjack game code

---
+++ EssentiBot/Games/BlackJack/Card.cs
@@ -0,0 +1,40 @@
+﻿namespace EssentiBot.Games.BlackJack
+{
+    enum Suit
+    {
+        Spades,
+        Hearths,
+        Clubs,
+        Diamonds,
+    };
+
+    enum Rank
+    {
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Quen,
+        King,
+    };
+
+
+    class Card
+    {
+        public Rank rank;
+        public Suit suit;
+
+        public Card(int Rank, int Suit)
+        {
+            rank = (Rank)Rank;
+            suit = (Suit)Suit;
+        }
+    }
+}
---
+++ EssentiBot/Games/BlackJack/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+
+namespace EssentiBot.Games.BlackJack
+{
+    class Player
+    {
+        public int handTotal = 0;
+        public string name;
+        public bool draw = true;
+        public bool Busted = false;
+        public List<Card> hand = new List<Card>();
+
+        public Player(string Name)
+        {
+            name = Name;
+        }
+
+        public void GetCards(List<Card> Deck)
+        {
+            hand.Add(Deck[0]);
+            Deck.RemoveAt(0);
+            _CalcHandTotal();
+        }
+
+        private void _CalcHandTotal()
+        {
+            handTotal = 0;
+            int aceCount = 0;
+            foreach (var card in hand)
+            {
+                if (card.rank == 0)
+                {
+                    aceCount++;
+                }
+                else if ((int)card.rank < 10)
+                {
+                    handTotal += (int)card.rank + 1;
+                }
+                else
+                {
+                    handTotal += 10;
+                }
+
+            }
+
+            if (aceCount > 0)
+            {
+                if (handTotal <= (11 - aceCount))
+                {
+                    handTotal += (aceCount - 1) + 11;
+                }
+                else
+                {
+                    handTotal += aceCount;
+                }
+            }
+
+        }
+    }
+}
